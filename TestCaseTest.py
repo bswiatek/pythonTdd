531eb072f3d0cf921f60a454a1b8af2b4ccfb590
Bartłomiej Świątek <emini1@swiatek.biz>
2017-03-21 17:41:38 +0100

    counting

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -1,5 +1,6 @@
 from TestCase import TestCase
 from WasRun import WasRun
+from TestResult import TestResult
 
 class TestCaseTest(TestCase):
 
@@ -11,4 +12,16 @@ class TestCaseTest(TestCase):
   test.run()
   assert("setUp testMethod tearDown " == test.log)
 
+ def testResult(self):
+  test= WasRun("testMethod")
+  result= test.run()
+  assert("1 run, 0 failed" == result.summary())
+
+ def testFailedResult(self):
+  test= WasRun("testBrokenMethod")
+  result= test.run()
+  assert("1 run, 1 failed" == result.summary())
+
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
